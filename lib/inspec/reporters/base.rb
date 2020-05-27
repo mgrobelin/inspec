abac606ae19f8e390cc5ed282756de1068ca1a79
Clinton Wolfe <clintoncwolfe@gmail.com>
2020-05-27 15:42:52 -0400

    DRY up reporter-trimming code by placing it in a mixin class
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/reporters/base.rb
+++ lib/inspec/reporters/base.rb
@@ -1,33 +1,17 @@
+require_relative "../utils/run_data_filters"
+
 module Inspec::Reporters
   class Base
+    include Inspec::Utils::RunDataFilters
+
     attr_reader :run_data
 
     def initialize(config)
       @config = config
-      @run_data = config[:run_data]
-      apply_report_resize_options unless @run_data.nil?
+      @run_data = config[:run_data] || {}
+      apply_run_data_filters_to_hash
+
       @output = ""
-    end
-
-    # Apply options such as message truncation and removal of backtraces
-    def apply_report_resize_options
-      runtime_config = Inspec::Config.cached.respond_to?(:final_options) ? Inspec::Config.cached.final_options : {}
-
-      message_truncation = runtime_config[:reporter_message_truncation] || "ALL"
-      trunc = message_truncation == "ALL" ? -1 : message_truncation.to_i
-      include_backtrace = runtime_config[:reporter_backtrace_inclusion].nil? ? true : runtime_config[:reporter_backtrace_inclusion]
-
-      @run_data[:profiles]&.each do |p|
-        p[:controls].each do |c|
-          c[:results]&.map! do |r|
-            r.delete(:backtrace) unless include_backtrace
-            if r.key?(:message) && r[:message] != "" && trunc > -1
-              r[:message] = r[:message][0...trunc] + "[Truncated to #{trunc} characters]"
-            end
-            r
-          end
-        end
-      end
     end
 
     def output(str, newline = true)
